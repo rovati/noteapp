290a05846ad59a79cc043275d9564357eca6a70a
rovati <lorenzo.rovati@tutanota.com>
2021-06-17 00:13:19 +0200

    Route note tile to note page

--- note_app/lib/widget/NoteTile.dart
+++ note_app/lib/widget/NoteTile.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:note_app/model/Note.dart';
 import 'package:note_app/model/NotesList.dart';
+import 'package:note_app/screen/NotePage.dart';
 
 class NoteTile extends StatefulWidget {
   final int noteID;
@@ -22,24 +24,24 @@ class _NoteTileState extends State<NoteTile> {
   Widget build(BuildContext context) {
     return Container(
         decoration: BoxDecoration(
-          //color: Colors.transparent,
           gradient: LinearGradient(colors: [
             Color.fromARGB(0x22, 0xC1, 0xC1, 0xC1),
             Color.fromARGB(0x22, 0xC1, 0xC1, 0xC1)
           ], begin: Alignment.topCenter, end: Alignment.bottomCenter),
-
-          /* border: Border.all(
-            color: Color.fromARGB(0xAA, 0xE1, 0x55, 0x54),
-            width: 2,
-          ), */
           borderRadius: BorderRadius.circular(20),
         ),
         child: ListTile(
-          onTap: () {},
+          onTap: _onTap,
           leading: Icon(Icons.dehaze_outlined),
           title: Text(list.getTitleOf(widget.noteID)),
           minVerticalPadding: 20,
           horizontalTitleGap: 10,
         ));
+  }
+
+  void _onTap() {
+    Note note = Note(0, 'Note title', content: 'test content');
+    Navigator.push(
+        context, MaterialPageRoute(builder: (context) => NotePage(note: note)));
   }
 }
