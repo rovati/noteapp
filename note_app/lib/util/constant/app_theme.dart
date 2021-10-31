2be9bd27757b4d394c7b3baadf4be5dc6a8a3a97
rovati <53517598+rovati@users.noreply.github.com>
2021-10-31 22:52:42 +0100

    Remove firebase files

--- note_app/lib/util/constant/app_theme.dart
+++ note_app/lib/util/constant/app_theme.dart
@@ -4,7 +4,7 @@ class Themes {
   static final defaultTheme = ThemeData(
     brightness: Brightness.dark,
     primaryColor: Color.fromARGB(0xFF, 0xE1, 0x55, 0x54),
-    accentColor: Color.fromARGB(0xFF, 0xE1, 0x55, 0x54),
+    // accentColor: Color.fromARGB(0xFF, 0xE1, 0x55, 0x54),
   );
 
   static final red = Color.fromARGB(0xFF, 0xE1, 0x55, 0x54);
