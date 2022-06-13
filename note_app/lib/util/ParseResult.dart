7306021a48a0d9b79527d233bda95b0b3c039281
rovati <lorenzo.rovati@tutanota.com>
2022-06-13 19:02:53 +0200

    Forgor new file

---
+++ note_app/lib/util/ParseResult.dart
@@ -0,0 +1,26 @@
+import 'package:note_app/model/Note.dart';
+
+class ParseResult {
+  List<Note> parsed;
+  List<String> unparsed;
+
+  ParseResult()
+      : parsed = [],
+        unparsed = [];
+
+  void addParsed(Note note) {
+    parsed.add(note);
+  }
+
+  void addAllParsed(List<Note> notes) {
+    parsed.addAll(notes);
+  }
+
+  void addUnparsed(String s) {
+    unparsed.add(s);
+  }
+
+  void addAllUnparsed(List<String> ls) {
+    unparsed.addAll(ls);
+  }
+}
