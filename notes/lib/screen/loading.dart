edcce9effed3fdecc86150c37a5ef920bc3f72d3
Lorenzo Rovati <lorenzo.rovati@tutanota.com>
2022-12-24 18:13:33 +0100

    Add note models and theme values

---
+++ notes/lib/screen/loading.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class LoadingScreen extends StatelessWidget {
+  const LoadingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) => const Scaffold();
+}
---
+++ notes/lib/theme/app_theme.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class Themes {
+  static final defaultTheme = ThemeData(
+    brightness: Brightness.dark,
+    primaryColor: const Color.fromARGB(0xFF, 0xE1, 0x55, 0x54),
+    // accentColor: Color.fromARGB(0xFF, 0xE1, 0x55, 0x54),
+  );
+
+  static const red = Color.fromARGB(0xFF, 0xE1, 0x55, 0x54);
+  static const lightBlue = Color.fromARGB(0xFF, 0x8E, 0xC4, 0xFF);
+  static const grey = Color.fromARGB(0xFF, 0x20, 0x20, 0x20);
+  static const tileBg = Color.fromARGB(0x33, 0x29, 0x29, 0x29);
+  static const plaintextBg = Color.fromARGB(0x13, 0x29, 0x29, 0x29);
+  static const titleBg = Color.fromARGB(0x10, 0xFF, 0xFF, 0xFF);
+  static const titleOutline = Color.fromARGB(0x70, 0xFF, 0xFF, 0xFF);
+
+  static final Widget background = Container(
+    decoration: const BoxDecoration(
+        gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+          red,
+          lightBlue,
+        ])),
+  );
+}
