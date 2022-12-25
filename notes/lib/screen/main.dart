42283d8d737ece9236d7cf1ac1231824b36aa027
Lorenzo Rovati <lorenzo.rovati@tutanota.com>
2022-12-25 17:51:08 +0100

    Add main page

---
+++ notes/lib/screen/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+import '../theme/app_theme.dart';
+import '../widget/dimsissible_notes.dart';
+import '../widget/toolbar.dart';
+
+/// Shows the list of notes and a bar that allows to create notes and access the
+/// settings page, plus other thingies
+class MainPage extends StatelessWidget {
+  const MainPage({key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Themes.background,
+          Center(
+            child: Container(
+              alignment: Alignment.center,
+              width: MediaQuery.of(context).size.width * 0.90,
+              child: const DismissibleNotes(),
+            ),
+          ),
+          const Align(
+            alignment: Alignment.bottomRight,
+            child: Padding(
+              padding: EdgeInsets.only(bottom: 15, right: 15),
+              child: Toolbar(),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
