3ca421f4b10172d7ea7028f590c40522fffc0e33
Lorenzo Rovati <lorenzo.rovati@tutanota.com>
2022-12-28 12:59:37 +0100

    Remove android code

--- note_app/lib/util/constant/app_values.dart
+++
@@ -1,6 +0,0 @@
-class Values {
-  static const int MAX_NOTES = 50;
-  static const String NOTES_DIR = 'notes';
-  static const String ORDERING_DIR = 'ordering';
-  static const String TEMP_DIR = 'temp';
-}
